b1c4103b31ad505020b32bd983a21ba449bfff36
Jim Burch <jrburch1@gmail.com>
2021-03-30 16:20:42 -0700

    updated axios

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Input from './Input.js';
+
+// axios requests here
+
 
 const App = () => {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Before Dark</h1>
+      <Input />
     </div>
   );
 };
