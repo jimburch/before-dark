04c4a145f73b7c0d326446299456e85e8e009e2e
Jim Burch <jrburch1@gmail.com>
2021-03-30 15:12:38 -0700

    set up mongoose

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('../database/index.js');
 
 const app = express();
-const PORT = 8080;
+const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  console.log('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`)
