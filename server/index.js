f36550399473b60ab00cd982bf39fa1f5037be27
Jim Burch <jrburch1@gmail.com>
2021-03-30 19:28:04 -0700

    set up router for sunset

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('../database/index.js');
+const getSunset = require('./controllers/sunset.js')
 
 const app = express();
 const PORT = 3001;
@@ -10,9 +11,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  console.log('Hello World!')
+  res.send('Hello World');
 })
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`)
 });
+
+// ROUTERS
+
+app.get('/sunset/:zip', getSunset);
