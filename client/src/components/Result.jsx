16e1ac5444d12368b3237af2738e160b8515dcc8
Jim Burch <jrburch1@gmail.com>
2021-04-01 10:12:32 -0700

    styling

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Result = ({ sunset, runTime, leaveTime }) => (
   <div>
         <div className="result-sub">Tonight's sunset is at {sunset}</div>
-        <div className="result-main">Leave by {leaveTime} to get home before dark!</div>
+        <div className="result-main">Start your run at {leaveTime} to get home before dark!</div>
         <div className="footnote">This app uses Google Maps geolocation API and Sunset Sunrise API. Developed by Jim Burch. Ideation & logo by Nik Donovic.</div>
   </div>
 );
