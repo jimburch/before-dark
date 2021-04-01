91855e8ca63a44a14b38633783fd65a91442653b
Jim Burch <jrburch1@gmail.com>
2021-04-01 11:46:19 -0700

    server adjustments

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('../database/index.js');
 const getSunset = require('./controllers/sunset.js')
 const storeLeaveStats = require('../database/controllers/leave.js');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`)
 });
 
 // ROUTERS
+
+// app.get('/', )
 
 app.get('/sunset/:zip', getSunset);
 app.post('/leave', storeLeaveStats);
