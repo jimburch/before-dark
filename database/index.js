0e747b13544cd917568202765c4fdd0c6addd5c6
Jim Burch <jrburch1@gmail.com>
2021-04-25 23:32:56 -0700

    changed mongo to atlas

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const config = require('../config.js');
 
-mongoose.connect('mongodb://localhost/before-dark', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://jim:${config.MONGO}@before-dark.ygq6e.mongodb.net/before-dark`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', () => { console.log('Mongoose connection error'); });
