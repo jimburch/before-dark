db4911526eafa420d5cda01e3890675307494627
Jim Burch <jrburch1@gmail.com>
2022-04-05 15:01:11 -0700

    rename server functions and remove unused db

--- database/controllers/leave.js
+++
@@ -1,9 +0,0 @@
-const addLeave = require('../models/leave.js');
-
-const storeLeaveStats = (req, res) => {
-  addLeave(req.body, (err, results) => {
-    err ? res.sendStatus(500) : res.send(results);
-  })
-};
-
-module.exports = storeLeaveStats;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const database = require('../database/index.js');
-const getSunset = require('./controllers/sunset.js')
-const storeLeaveStats = require('../database/controllers/leave.js');
+const getSunset = require('./controllers/sunset.js');
 
 const app = express();
 const PORT = 8000;
@@ -11,10 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT:${PORT}`)
+	console.log(`Server listening on PORT:${PORT}`);
 });
 
 // ROUTERS
-
 app.get('/sunset/:zip', getSunset);
-app.post('/leave', storeLeaveStats);
