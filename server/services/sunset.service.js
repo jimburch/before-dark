924aec9ebc9e9ebe7e46fe5d8dbcfcb44cc44b4f
Jim Burch <jrburch1@gmail.com>
2022-04-05 15:12:33 -0700

    update token envs

--- server/services/sunset.service.js
+++ server/services/sunset.service.js
@@ -1,19 +1,25 @@
 const axios = require('axios');
-const config = require('../../config.js');
+
+const TOKEN = GOOGLE_MAPS_API_KEY.process.env;
 
 const getCoordsAndSunset = (data, callback) => {
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${data}&key=${config.TOKEN}`)
-  .then((response) => {
-    const lat = response.data.results[0].geometry.location.lat;
-    const lng = response.data.results[0].geometry.location.lng;
-    return axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`)
-  })
-  .then((response) => {
-    callback(null, response.data.results.sunset);
-  })
-  .catch((err) => {
-    callback(err);
-  })
-}
+	axios
+		.get(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${data}&key=${config.TOKEN}`
+		)
+		.then(response => {
+			const lat = response.data.results[0].geometry.location.lat;
+			const lng = response.data.results[0].geometry.location.lng;
+			return axios.get(
+				`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
+			);
+		})
+		.then(response => {
+			callback(null, response.data.results.sunset);
+		})
+		.catch(err => {
+			callback(err);
+		});
+};
 
 module.exports = getCoordsAndSunset;
